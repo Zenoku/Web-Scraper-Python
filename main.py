7c5e621eb17282c27374b3b30bafadd34ed53d92
Zenoku <zenoku132@protonmail.com>
2022-03-19 23:18:47 -0400

    inserted primative error handling as well as some soup stuff

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-from urllib.request import urlopen
+import requests
 from bs4 import BeautifulSoup
 
 # request a url
 url = input("Insert URL:")
-page = urlopen(url)
 
-# extract and decode the html
-html = page.read().decode("utf-8")
+try:
+    page = requests.get(url)
+except:
+    print("Not a valid url")
+    exit()
 
-# create soup object (the actual webpage w/o the html)
-soup = BeautifulSoup(html, "html.parser")
-print(soup.get_text())
+# create soup object 
+soup = BeautifulSoup(page.text, "html.parser")
+
+# stuff to get
+title = soup.title.string
+print(title)
+
+authors = soup.find("meta", property = "author")
+print(authors)
